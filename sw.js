a12cbec0b198b302faf1da5dc7600f4069471eb4
zsjappreciatelyq-coder <zsjappreciatelyq@gmail.com>
2026-08-18 13:40:55 +0800

    Add offline cache service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='jingting-v1';
+const FILES=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
